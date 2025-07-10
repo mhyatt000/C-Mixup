2587045e6a0ebff23967b7f402ec75053d93ee7e
matt hyatt <mhyatt000>
2025-07-10 14:41:23 -0500

    remove povertymap echonet

--- PovertyMap/main.sh
+++
@@ -1,2 +0,0 @@
-python main.py --dataset poverty --algorithm mixup --data-dir ../../datasets/ --experiment_dir .. --is_kde 1 --kde_bandwidth 0.5
-
--- PovertyMap/models/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .poverty import Model as poverty
-
-from .poverty import NUM_CLASSES as poverty_n_class
-
-__all__ = [poverty]
